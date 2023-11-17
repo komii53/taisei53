9531756df6589c503afa18cebd0aadc18908abc5
komii53 <148539062+komii53@users.noreply.github.com>
2023-11-17 19:35:47 +0900

    Add files via upload

---
+++ 大文字小文字.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	char str1[] = a;
+	char str2[] = b;
+	char* result = strstr(str1, str2);
+	
+	printf("•¶Žš‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+	scanf_s("%c", &a);
+
+	(b>='A')&&(b<='Z')
+
+	if (result != NULL) {
+		printf("Found:%s\n", result);
+	}
+	else {
+		printf("Not found\n");
+	}
+
+	return 0;
+}
